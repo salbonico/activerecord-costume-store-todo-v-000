b191e7ec9d7e9f631fb3909b062caf82169def63
Steve Albonico <salbonico@gmail.com>
2019-01-31 01:21:52 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,7 +11,12 @@
 class CreateCostumes < ActiveRecord::Migration[5.1]
 
 def change
-
+  create_table :costumes do |t|
+    t.string :name
+    t.float :price
+    t.string :image_url
+    t.string :size
+    t.timestamps
   end
 
 end
