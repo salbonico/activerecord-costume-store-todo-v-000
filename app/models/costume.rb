05670b4181b9f74096c45d7ac8e758166b2c3f40
Steve Albonico <salbonico@gmail.com>
2019-01-31 01:21:18 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -2,12 +2,5 @@
 # It should inherit from ActiveRecord::Base
 class Costume < ActiveRecord::Base
 
-  drop_table :costumes
-  create_table :costumes do |t|
-    t.string :name
-    t.float :price
-    t.string :image_url
-    t.string :size
-    t.timestamps
-end
+  
 end
