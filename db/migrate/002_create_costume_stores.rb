4a5c9302fbeb3760d0e812b4e04bf20d6f5d18b0
Steve Albonico <salbonico@gmail.com>
2019-01-31 01:37:26 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,7 +1,7 @@
 class CreateCostumeStores < ActiveRecord::Migration[5.1]
 
   def change
-
+    drop_table :costume_stores
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
